023a831ec64b6f5dad883b1547dc961211b08ba4
Bryan Daniels <bryan.daniels.1@asu.edu>
2020-03-26 17:21:48 -0700

    add test for parallel running of generateEnsemble

--- test/test_parallel.py
+++ test/test_parallel.py
@@ -12,29 +12,59 @@ import SirIsaac.fakeData
 
 NUMPROCS = 2
 
+def test_data_and_model():
+    
+    # create simple SloppyCell fitting model with single independent parameter
+    complexity = 1
+    indepParamNames = ['a']
+    outputNames = ['b']
+    m = SirIsaac.fittingProblem.CTSNFittingModel(complexity,
+        indepParamNames,outputNames)
+    
+    # create a single set of fake data (with small added noise and constant seed)
+    numPoints = 3
+    timeInterval = [0,10]
+    noiseFracSize = 0.1 # 0.01 hangs for local_fit_parallel??
+    data = [SirIsaac.fakeData.noisyFakeData(m.net,
+                                            numPoints,
+                                            timeInterval,
+                                            noiseFracSize=noiseFracSize,
+                                            seed=0)]
+                                       
+    indepParamsList = [[1.] for d in data]
+    dataModel = m._SloppyCellDataModel(data,indepParamsList)
+                                       
+    return m,data,indepParamsList,dataModel
+
 class TestParallel(unittest.TestCase):
+
+    def test_generate_ensemble_parallel(self):
+        
+        m,data,indepParamsList,dataModel = test_data_and_model()
+        
+        # set up ensemble generation
+        p = m.getParameters()
+        totalSteps = 25
+        keepSteps = 5
+        seeds = (1,1)
+        ensGen = SirIsaac.fittingProblem.EnsembleGenerator(totalSteps,keepSteps,
+                                                           seeds=seeds)
+        
+        # first run serially
+        ensSerial,_ = ensGen.generateEnsemble(dataModel,p)
+        # then run in parallel
+        ensParallel,_ = ensGen.generateEnsemble_pypar(NUMPROCS,dataModel,p)
+        
+        # check that we get the same answer when running in parallel
+        for paramIndex in range(len(ensSerial[0])):
+            self.assertAlmostEqual(ensSerial[0][paramIndex],
+                                   ensParallel[0][paramIndex])
+
     def test_local_fit_parallel(self):
         
-        # create simple SloppyCell fitting model
-        complexity = 1
-        indepParamNames = ['a']
-        outputNames = ['b']
-        m = SirIsaac.fittingProblem.CTSNFittingModel(complexity,
-            indepParamNames,outputNames)
-        
-        # create some fake data (with small added noise and constant seed)
-        numPoints = 3
-        timeInterval = [0,10]
-        noiseFracSize = 0.1 # 0.01 hangs??
-        data = [SirIsaac.fakeData.noisyFakeData(m.net,
-                                                numPoints,
-                                                timeInterval,
-                                                noiseFracSize=noiseFracSize,
-                                                seed=0)]
+        m,data,indepParamsList,dataModel = test_data_and_model()
         
         # set up fit
-        indepParamsList = [[1.]]
-        dataModel = m._SloppyCellDataModel(data,indepParamsList)
         p = m.getParameters()
         
         # first run serially
