bb91013254241bf5d5afd8250e8c1ffd7094a2e3
Bryan Daniels <bryan.daniels.1@asu.edu>
2020-06-03 08:29:26 -0700

    add more thorough tests for parallel functionality

--- test/test_parallel.py
+++ test/test_parallel.py
@@ -9,15 +9,19 @@ import unittest
 
 import SirIsaac.fittingProblem
 import SirIsaac.fakeData
+from SirIsaac.simplePickle import load,save
+import subprocess
+import os
 
 NUMPROCS = 2
+SIRISAACDIR = SirIsaac.fittingProblem.SIRISAACDIR
 
-def test_data_and_model():
+def example_data_and_model():
     
     # create simple SloppyCell fitting model with single independent parameter
     complexity = 1
-    indepParamNames = ['a']
-    outputNames = ['b']
+    indepParamNames = ['input']
+    outputNames = ['output']
     m = SirIsaac.fittingProblem.CTSNFittingModel(complexity,
         indepParamNames,outputNames)
     
@@ -25,44 +29,53 @@ def test_data_and_model():
     numPoints = 3
     timeInterval = [0,10]
     noiseFracSize = 0.1 # 0.01 hangs for local_fit_parallel??
+    lenData = 1 # (2 is probably a better test for parallel stuff in generateEnsemble)
     data = [SirIsaac.fakeData.noisyFakeData(m.net,
                                             numPoints,
                                             timeInterval,
                                             noiseFracSize=noiseFracSize,
-                                            seed=0)]
+                                            seed=seed) for seed in range(lenData)]
                                        
-    indepParamsList = [[1.] for d in data]
+    indepParamsList = [[1.+i] for i in range(lenData)]
     dataModel = m._SloppyCellDataModel(data,indepParamsList)
                                        
     return m,data,indepParamsList,dataModel
 
+
 class TestParallel(unittest.TestCase):
 
-    def test_generate_ensemble_parallel(self):
+    def test_basic_mpi_functionality(self):
+        """
+        Test basic MPI functionality
+        """
+        temp_input_filename = os.path.join(SIRISAACDIR,
+                                           "temporary_input_{}.dat".format(os.getpid()))
+        temp_output_filename = os.path.join(SIRISAACDIR,
+                                            "temporary_output_{}.dat".format(os.getpid()))
+        temp_stdout_filename = os.path.join(SIRISAACDIR,
+                                            "temporary_stdout_{}.txt".format(os.getpid()))
         
-        m,data,indepParamsList,dataModel = test_data_and_model()
-        
-        # set up ensemble generation
-        p = m.getParameters()
-        totalSteps = 25
-        keepSteps = 5
-        seeds = (1,1)
-        ensGen = SirIsaac.fittingProblem.EnsembleGenerator(totalSteps,keepSteps,
-                                                           seeds=seeds)
-        
-        # first run serially
-        ensSerial,_ = ensGen.generateEnsemble(dataModel,p)
-        # then run in parallel
-        ensParallel,_ = ensGen.generateEnsemble_pypar(NUMPROCS,dataModel,p)
-        
-        # check that we get the same answer when running in parallel
-        for paramIndex in range(len(ensSerial[0])):
-            self.assertAlmostEqual(ensSerial[0][paramIndex],
-                                   ensParallel[0][paramIndex])
+        input_data = {'test':123,'output_filename':temp_output_filename}
+        save(input_data,temp_input_filename)
+        stdoutFile = open(temp_stdout_filename,'w')
+        subprocess.call([ "mpirun","-np",str(NUMPROCS),"python",
+                          os.path.join(SIRISAACDIR, "mpi_basic.py"),
+                          temp_input_filename ],
+                          stderr=stdoutFile,stdout=stdoutFile,
+                          env=os.environ)
+        output_data = load(temp_output_filename)
+        os.remove(temp_input_filename)
+        os.remove(temp_output_filename)
+        os.remove(temp_stdout_filename)
+
+        self.assertTrue(input_data['test'] == output_data['test_output'])
 
     def test_local_fit_parallel(self):
+        """
+        Test localFitToData_parallel
+        """
         
-        m,data,indepParamsList,dataModel = test_data_and_model()
+        m,data,indepParamsList,dataModel = example_data_and_model()
         
         # set up fit
         p = m.getParameters()
@@ -81,5 +94,32 @@ class TestParallel(unittest.TestCase):
                             for result in outputDictParallel.values() ]
         for varParallel in varParallelList:
             self.assertAlmostEqual(varSerial,varParallel)
+
+    def test_generate_ensemble_parallel(self):
+        """
+        Test generateEnsemble_parallel
+        """
+
+        m,data,indepParamsList,dataModel = example_data_and_model()
+
+        # set up ensemble generation
+        p = m.getParameters()
+        totalSteps = 25
+        keepSteps = 5
+        seeds = (1,1)
+        ensGen = SirIsaac.fittingProblem.EnsembleGenerator(totalSteps,keepSteps,
+                                                           seeds=seeds)
+
+        # first run serially
+        ensSerial,_ = ensGen.generateEnsemble(dataModel,p)
+        # then run in parallel
+        ensParallel,_ = ensGen.generateEnsemble_parallel(NUMPROCS,dataModel,p)
+
+        # check that we get the same answer when running in parallel
+        for paramIndex in range(len(ensSerial[0])):
+            self.assertAlmostEqual(ensSerial[0][paramIndex],
+                                   ensParallel[0][paramIndex])
+
+    
         
         
