2b234aa29a947f0b229579a247a491090eadc9a2
Bryan Daniels <bryan.daniels.1@asu.edu>
2022-04-14 14:15:00 -0700

    update simplePickle for basic python 2 & 3 compatability

--- SirIsaac/simplePickle.py
+++ SirIsaac/simplePickle.py
@@ -7,17 +7,28 @@
 import io, sys
 import pickle
 
-# include some old name variants for back-compatability
-#import fittingProblem
-#sys.modules['FittingProblem'] = fittingProblem
+if sys.version_info.major > 2:
+    # Python 3
+    import pickle
+else:
+    # Python 2
+    import cPickle as pickle
 
 def save(obj,filename):
     fout = io.open(filename,'wb')
+    # Note: we currently save using backward-compatible protocol 2
     pickle.dump(obj,fout,2)
     fout.close()
 
 def load(filename):
     fin = io.open(filename,'rb')
-    obj = pickle.load(fin)
+    try:
+        obj = pickle.load(fin)
+    except UnicodeDecodeError:
+        # try using backward-compatible encoding in case
+        # the file was saved using python 2
+        fin.close()
+        fin = io.open(filename,'rb')
+        obj = pickle.load(fin,encoding='bytes')
     fin.close()
     return obj
