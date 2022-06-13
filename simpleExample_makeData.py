23b1a86c5f9b98267e04b95800b745a9ba1f5709
Bryan Daniels <bryan.daniels.1@asu.edu>
2022-06-13 14:53:05 -0700

    update simpleExample to be simpler and quicker to run

--- simpleExample_makeData.py
+++ simpleExample_makeData.py
@@ -2,44 +2,47 @@
 #
 # Bryan Daniels
 # 1.29.2014
+# updated 2022/6/13
 #
 # Construct fake data to be used in
 # simpleExample.ipynb.
 #
 # Data taken at times t sampled uniformly between
-# 0 and 1 and initial conditions x0 sampled
-# uniformly between 1 and 2.
+# 0 and 1, initial conditions x0 sampled
+# uniformly between 1 and 2, and decay rate tau
+# sampled uniformly between 0 and 1.
 #
-# Output is a sinusoid varying between 1 and 2
-# with the given initial condition x0.
+# Output is an exponential decay with the given
+# initial condition x0 and decay rate tau:
 #
-# x = 3/2 + 1/2 sin( 4 pi t + arcsin(2x0 - 3) ) + N(0,0.1)
+# x = x_0 * exp(-t/tau) + N(0,0.1)
 #
 
-import scipy
+import numpy as np
 
 numSamples = 100
 noiseStd = 0.1
 
-scipy.random.seed(10000)
-times = scipy.random.rand(numSamples)
-noises = scipy.random.normal(scale=noiseStd,size=numSamples)
-x0s = 1. + scipy.random.rand(numSamples)
-xs = 1.5 + 0.5 * scipy.sin( 4.*scipy.pi*times \
-                            + scipy.arcsin(2.*x0s - 3.) )
+np.random.seed(10000)
+times = np.random.rand(numSamples)
+noises = np.random.normal(scale=noiseStd,size=numSamples)
+x0s = 1. + np.random.rand(numSamples)
+taus = np.random.rand(numSamples)
+xs = x0s * np.exp(-times/taus)
 xsNoisy = xs + noises
 
 # in the output file,
-# column 1: initial condition
-# column 2: measurement time
-# column 3: measurement value
-# column 4: measurement uncertainty (standard deviation)
+# column 1: initial condition x0
+# column 2: decay rate tau
+# column 3: measurement time
+# column 4: measurement value
+# column 5: measurement uncertainty (standard deviation)
 
-noiseStds = scipy.repeat(noiseStd,100)
+noiseStds = np.repeat(noiseStd,100)
 
-data = list(zip( x0s, times, xsNoisy, noiseStds ))
+data = list(zip( x0s, taus, times, xsNoisy, noiseStds ))
 
-scipy.savetxt('simpleExample_data.txt',data)
+np.savetxt('simpleExample_data.csv',data,delimiter=',')
 
 
 
