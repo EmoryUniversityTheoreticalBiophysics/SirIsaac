a701545477a0fb0a08f4959cb07cddafc8977f70
Bryan Daniels <bryan.daniels.1@asu.edu>
2022-06-13 15:28:44 -0700

    remove a few references to pypar and replace with mpi4py

--- setup.py
+++ setup.py
@@ -12,12 +12,11 @@ setup(
         'SloppyCell',
     ],
     extras_require={
-        'multi-processor': ['pypar'],
+        'multi-processor': ['mpi4py'],
         'graphs': ['pygraphviz'],
         'SBML': ['python-libsbml'],
     },
     dependency_links=[
-        'https://github.com/daleroberts/pypar/tarball/master#egg=pypar',
         'https://github.com/GutenkunstLab/SloppyCell/tarball/master#egg=SloppyCell',
     ]
 )
